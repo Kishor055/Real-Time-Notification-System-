2a35c36d9c897905abac6de58a3afc80a9606642
Gaurav Bramhane <bramhanegaurav50@gmail.com>
2026-05-24 08:28:12 +0000

    fix this

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,10 @@
-
 'use client';
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyDummyKey",
+  apiKey: "AIzaSyD-D-06eK0T0fK0T0fK0T0fK0T0fK0T0f",
   authDomain: "novapulse-platform.firebaseapp.com",
   projectId: "novapulse-platform",
   storageBucket: "novapulse-platform.firebasestorage.app",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef123456"
+  messagingSenderId: "249392254924",
+  appId: "1:249392254924:web:03e090df4a36283b631d86"
 };
